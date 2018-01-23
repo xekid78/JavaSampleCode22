483f5326210e80ce3eb31c2d79af9bbabc71fbb4
Makoto Tanabe <xekid78@outlook.com>
2018-01-23 12:10:04 +0900

    スコープの理解

---
+++ sample22.java
@@ -0,0 +1,25 @@
+/**
+ *
+ * @author xekid78
+ *
+ */
+public class sample22 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int gokei = sum(a, b);
+		System.out.println("変数aは" + a + "です。");
+		System.out.println("変数bは" + b + "です。");
+		System.out.println("合計は" + gokei + "です。");
+
+	}
+
+	public static int sum(int x, int y) {
+		int a = 5;
+		System.out.println("変数aは" + a + "です。");
+		return x + y;
+
+	}
+
+}
